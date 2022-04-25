6a82b4a5bcf24e1bd3e462b903cb98c3c593cab7
houdini91 <mdstrauss91@gmail.com>
2022-04-25 11:13:00 +0300

    fix duplicate classifications per coordinates
    Signed-off-by: houdini91 <mdstrauss91@gmail.com>

--- syft/file/classification_cataloger.go
+++ syft/file/classification_cataloger.go
@@ -17,7 +17,7 @@ func NewClassificationCataloger(classifiers []Classifier) (*ClassificationCatalo
 
 func (i *ClassificationCataloger) Catalog(resolver source.FileResolver) (map[source.Coordinates][]Classification, error) {
 	results := make(map[source.Coordinates][]Classification)
-
+	classResult := make(map[string]*Classification)
 	numResults := 0
 	for _, location := range allRegularFiles(resolver) {
 		for _, classifier := range i.classifiers {
@@ -26,9 +26,17 @@ func (i *ClassificationCataloger) Catalog(resolver source.FileResolver) (map[sou
 				log.Warnf("file classification cataloger failed with class=%q at location=%+v: %+v", classifier.Class, location, err)
 				continue
 			}
+
 			if result != nil {
-				results[location.Coordinates] = append(results[location.Coordinates], *result)
-				numResults++
+				if val, ok := classResult[result.Class]; ok {
+					for k, v := range result.Metadata {
+						val.Metadata[k] = v
+					}
+				} else {
+					results[location.Coordinates] = append(results[location.Coordinates], *result)
+					classResult[result.Class] = result
+					numResults++
+				}
 			}
 		}
 	}

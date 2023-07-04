7c0b649ea24016ff9dc25662346fd7f4601c2a4f
Mikey Strauss <mdstrauss91@gmail.com>
2023-07-04 10:36:18 +0300

    remove vuln options

--- syft/pkg/package.go
+++ syft/pkg/package.go
@@ -89,7 +89,7 @@ func (p *Package) merge(other Package) error {
 	}
 
 	if p.PURL != other.PURL {
-		log.Warnf("merging packages have with different pURLs: %q=%q vs %q=%q", p.id, p.PURL, other.id, other.PURL)
+		log.Debugf("merging packages have with different pURLs: %q=%q vs %q=%q", p.id, p.PURL, other.id, other.PURL)
 	}
 
 	p.Locations.Add(other.Locations.ToSlice()...)

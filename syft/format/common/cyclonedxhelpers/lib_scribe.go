2f2e89d78d7e03636da67799869a7949aab773e9
houdini91 <mdstrauss91@gmail.com>
2023-12-25 10:28:30 +0200

    expose cdx translation

--- syft/format/common/cyclonedxhelpers/lib_scribe.go
+++ syft/format/common/cyclonedxhelpers/lib_scribe.go
@@ -8,3 +8,15 @@ import (
 func EncodeLicenses(p pkg.Package) *cyclonedx.Licenses {
 	return encodeLicenses(p)
 }
+
+func EncodeComponent(p pkg.Package) cyclonedx.Component {
+	return encodeComponent(p)
+}
+
+func EncodeAuthor(p pkg.Package) string {
+	return encodeAuthor(p)
+}
+
+func EncodePublisher(p pkg.Package) string {
+	return encodePublisher(p)
+}
